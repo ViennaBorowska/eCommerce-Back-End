c48ee000425a2736670013ba08dd36f78a0181e7
Vienna Borowska <vienna.borowska@hotmail.com>
2022-02-06 19:19:37 +0000

    Added USE DATABASE to schema.sql. Modified wrong route text in route index.js

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+router.use('/', (req, res) => {
+  res.send(
+    `<h1>Oops, this isn't the right route!</h1><br><h2>Add routes in the address bar to see your database tables e.g. localhost:3001/api/products</h2><br><h1>Cool, huh?`
+  );
 });
 
 module.exports = router;
